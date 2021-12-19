a933e0cd9fe2cb550a8e7a04bf0e691fadd2d8e8
Nazim Hasan <nazimhasan124073@gmail.com>
2021-12-20 03:03:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AuthProvider from './context/AuthProvider';
+import DashboardMenu from './Pages/Dashboard/DashboardMenu/DashboardMenu';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import Navigation from './Pages/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+      <BrowserRouter>
+        <Navigation></Navigation>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/login">
+            <Login></Login>
+          </Route>
+          
+          <PrivateRoute path="/dashboard">
+              <DashboardMenu></DashboardMenu>
+            </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Pages/Dashboard/DashboardMenu/DashboardMenu.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { FaBars, FaUser, FaUserEdit } from "react-icons/fa";
+import { Link, Switch, useRouteMatch } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
+import AdminRoute from '../../Login/AdminRoute/AdminRoute';
+import DashboardHome from '../DashboardHome/DashboardHome';
+import EditEmployee from '../ManageEmployee/EditEmployee';
+import ManageEmployee from '../ManageEmployee/ManageEmployee';
+import MyProfile from '../MyProfile/MyProfile';
+
+
+const DashboardMenu = () => {
+    let { path, url } = useRouteMatch();
+    const {user,logOut,admin}=useAuth();
+    
+    return (
+
+
+            <div className="container-fluid">
+                <div className="row flex-nowrap">
+                <div className='dashboard'>
+                    <div className="dashboard-nav">
+                    <header>
+                        
+                        <Link to={`${url}`} className="brand-logo"><span>BRAND</span></Link></header>
+                         <nav className="dashboard-nav-list">
+                             <Link to={`${url}/dashboardHome`} className="dashboard-nav-item"><i><FaUser/></i> Dashboard </Link>
+                             <Link to={`${url}/profiles`} className="dashboard-nav-item"><i><FaUser/></i> Profile </Link>
+                             <Link to={`${url}/employee`} className="dashboard-nav-item"><i><FaUserEdit/></i>Employee </Link>
+
+                              <div className="nav-item-divider"></div>
+                              <Link to={`${url}`} className="dashboard-nav-item">
+                              <i className="fas fa-sign-out-alt"></i> Logout
+                              </Link>
+                         </nav>
+                    </div>
+
+                    <div className='dashboard-app'>
+                         <header className='dashboard-toolbar'>
+                              <a href="#!" className="menu-toggle"><i ><FaBars/></i></a>
+                         </header>
+                         <div className='dashboard-content'>
+                              
+                         
+                    <div className="col py-3">
+                        <Switch>
+                            <AdminRoute exact path={`${path}/dashboardHome`}>
+                            <DashboardHome></DashboardHome>
+                            </AdminRoute>
+                            <AdminRoute path={`${path}/profiles`}>
+                            <MyProfile></MyProfile>
+                            </AdminRoute>
+                            <AdminRoute path={`${path}/employee`}>
+                            <ManageEmployee></ManageEmployee>
+                            </AdminRoute>
+                            <AdminRoute path={`${path}/edit/:Eid`}>
+                            <EditEmployee></EditEmployee>
+                            </AdminRoute>
+
+                            {/* Admin Route  */}
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    );
+};
+
+export default DashboardMenu;
